08c56c0cf296c645f74a5c61a02e978917d09b36
jiangyigithub <15216787356@163.com>
2021-08-14 11:53:37 +0800

    update repo

---
+++ 59queue.cpp
@@ -0,0 +1,20 @@
+#include "head.hpp"
+class MaxQueue {
+    queue<int> que;
+    deque<int> deq;
+    public MaxQueue() {
+        
+    }
+    
+    public int max_value() {
+
+    }
+    
+    public void push_back(int value) {
+
+    }
+    
+    public int pop_front() {
+
+    }
+}
---
+++ head.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+using namespace std;
