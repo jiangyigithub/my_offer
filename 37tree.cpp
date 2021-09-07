8767012cb77be850c094cb1440c56cb13ddb1bca
jiangyigithub <15216787356@163.com>
2021-09-07 19:11:18 +0800

    update

--- 37tree.cpp
+++ 37tree.cpp
@@ -9,17 +9,95 @@
 
 class Codec {
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string str="123";
-        return str;
+        if (root == nullptr)return "[null]";//空指针直接返回
+        string ret = "[";
+        ret += to_string(root->val);
+        queue<TreeNode*> Q;
+        Q.push(root);
+        //节点入队时，将其值转换为字符串加入结果中
+        while (!Q.empty()) {
+            int sz = Q.size();
+            bool flag = false;//用于判断当前层是否是层序遍历的倒数第二层
+            for (int i = 0; i < sz; ++i) {
+                auto t = Q.front();
+                Q.pop();
+                if (t->left) {
+                    if (t->left->left || t->left->right)flag = true;//下下层有节点，故当前层不是倒数第二层
+                    Q.push(t->left);
+                    ret.push_back(',');
+                    ret += to_string(t->left->val);
+                }
+                else {
+                    ret += ",null";
+                }
+                if (t->right) {
+                    if (t->right->left || t->right->right)flag = true;//下下层有节点，故当前层不是倒数第二层
+                    Q.push(t->right);
+                    ret.push_back(',');
+                    ret += to_string(t->right->val);
+                }
+                else {
+                    ret += ",null";
+                }
+            }
+            if (flag == false)break;//当前层是倒数第二层，跳出
+        }
+        ret.push_back(']');
+        return ret;
     }
+    
+        //       1
+        //      / \
+        //     2   3
+        //        / \
+        //       4   5
+       
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        
+        int len = data.length();
+        int i = 1, j = data.find_first_of(',');
+        if (j == -1)return nullptr;
+        vector<int> store;//存储节点值
+        vector<bool> record;//记录树的层序遍历结构，true为非空节点，false为空节点
+        for (; j < len - 1 && i < len - 1; ) {
+            if (data[i] == 'n') {
+                i += 5;
+                record.push_back(false);
+                continue;
+            }
+            record.push_back(true);
+            j = i + 1;
+            while (j < len && data[j] >= '0' && data[j] <= '9') {
+                ++j;
+            }
+            store.push_back(stoi(data.substr(i, j - i)));
+            i = j + 1;
+        }
+        int index = 0;
+        int sz = record.size();
+        TreeNode* root = new TreeNode(store[index++]);
+        queue<TreeNode*> Q;
+        Q.push(root);
+        //按照层序遍历的顺序进行“反序列化”
+        for (int i = 1; i < sz - 1; i += 2) {
+            auto t = Q.front();
+            Q.pop();
+            if (record[i]) {
+                t->left = new TreeNode(store[index++]);
+                Q.push(t->left);
+            }
+            if (record[i + 1]) {
+                t->right = new TreeNode(store[index++]);
+                Q.push(t->right);
+            }
+        }
+        return root;
     }
+
+
 };
 
 // Your Codec object will be instantiated and called as such:
