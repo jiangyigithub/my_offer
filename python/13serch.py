14eab78fba71b2fff579d77737ee580a3d0a91aa
JIANG Yi (XC/PJ-ICV-CN) <jan5szh@SZHPC12289.cn.bosch.com>
2021-09-26 03:47:32 -0400

    update

--- python/13serch.py
+++ python/13serch.py
@@ -3,35 +3,20 @@ class Solution:
         queue, visited = [(0, 0, 0, 0)], set()
         while queue:
             i, j, si, sj = queue.pop(0)
-            if i >= m or j >= n or k < si + sj or (i, j) in visited: continue
-            visited.add((i,j))
+            if i >= m or j >= n or k < si + sj or (i, j) in visited:
+                continue
+            visited.add((i, j))
             queue.append((i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj))
             queue.append((i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8))
         return len(visited)
 
 def main():
-    import sys
-    import io
-    def readlines():
-        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
-            yield line.strip('\n')
-
-    lines = readlines()
-    while True:
-        try:
-            line = next(lines)
-            m = int(line)
-            line = next(lines)
-            n = int(line)
-            line = next(lines)
-            k = int(line)
-            
-            ret = Solution().movingCount(m, n, k)
-
-            out = str(ret)
-            print(out)
-        except StopIteration:
-            break
+    m = int(2)
+    n = int(3)
+    k = int(1)
+    ret = Solution().movingCount(m, n, k)
+    out = str(ret)
+    print(out)
 
 if __name__ == '__main__':
     main()
