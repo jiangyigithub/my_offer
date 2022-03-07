156e866e8e5577736bbfd97814b473b727329eb7
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-07 17:15:59 +0800

    ++

--- python/29spiral.py
+++ python/29spiral.py
@@ -1,12 +1,17 @@
 class Solution:
     def spiralOrder(self, matrix: [[int]]) -> [int]:
+        # 非预期输入检测
         if not matrix:
             return []
+        # 边界初始值
         l, r, t, b, res = 0, len(matrix[0]) - 1, 0, len(matrix) - 1, []
         while True:
+            # 打印"一行"
             for i in range(l, r + 1):
                 res.append(matrix[t][i])  # left to right
+            # 边界更新
             t += 1
+            # 边界干涉,循环退出
             if t > b:
                 break
             for i in range(t, b + 1):
