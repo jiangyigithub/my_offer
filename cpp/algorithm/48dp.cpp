593a28e4b5f0809741939f76324e4d1c14302e03
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-21 11:54:50 +0800

    update

--- cpp/algorithm/48dp.cpp
+++ cpp/algorithm/48dp.cpp
@@ -7,10 +7,12 @@ public:
         unordered_map<char, int> dic;
         int i = -1, res = 0, len = s.size();
         for (int j = 0; j < len; j++)
-        {
+        {   
+            // 什么时候更新左指针
             if (dic.find(s[j]) != dic.end())
                 i = max(i, dic.find(s[j])->second); // 更新左指针
             dic[s[j]] = j;                          // 哈希表记录
+            // j-i 为不含重复字符串的首尾指针
             res = max(res, j - i);                  // 更新结果
         }
         return res;
