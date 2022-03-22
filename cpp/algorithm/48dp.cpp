9948c095754417188e6085ad5c7e21d912378038
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-22 10:55:47 +0800

    update

--- cpp/algorithm/48dp.cpp
+++ cpp/algorithm/48dp.cpp
@@ -10,10 +10,10 @@ public:
         int i = -1, res = 0, len = s.size();
         for (int j = 0; j < len; j++)
         {   
-            // 什么时候更新左指针
+            // 判断元素是否在哈希表中
             if (dic.find(s[j]) != dic.end())
-                i = max(i, dic[s[j]]); // 更新左指针
-            dic[s[j]] = j;                          // 哈希表记录
+                i = max(i, dic[s[j]]);              // 元素在哈希表中上一时刻的位置
+            dic[s[j]] = j;                          // 元素在哈希表中当前时刻的位置
             // j-i 为不含重复字符串的首尾指针
             res = max(res, j - i);                  // 更新结果
         }
