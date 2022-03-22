0e3700ee8a724ed9d8030d4e7810e603615dba78
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-22 10:50:12 +0800

    update

--- cpp/algorithm/48dp.cpp
+++ cpp/algorithm/48dp.cpp
@@ -1,4 +1,6 @@
 #include "head.hpp"
+/// unordered_map 的使用方法： find ,判断元素在不在hash_map中
+/// 空间复杂度的优化
 class Solution
 {
 public:
@@ -10,7 +12,7 @@ public:
         {   
             // 什么时候更新左指针
             if (dic.find(s[j]) != dic.end())
-                i = max(i, dic.find(s[j])->second); // 更新左指针
+                i = max(i, dic[s[j]]); // 更新左指针
             dic[s[j]] = j;                          // 哈希表记录
             // j-i 为不含重复字符串的首尾指针
             res = max(res, j - i);                  // 更新结果
