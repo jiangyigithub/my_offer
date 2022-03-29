26a8d9805eb33a33acf68a6a49f03ae3257e7ddb
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-29 17:24:51 +0800

    update stl

---
+++ cpp/stl/stl01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+    vec.push_back(4);
+    vec.push_back(1);
+    vec.push_back(8);
+
+    vector<int>::iterator itr1 = vec.begin();
+    vector<int>::iterator itr2 = vec.end();
+
+    for (vector<int>::iterator itr = itr1; itr != itr2; ++itr)
+    {
+        cout << *itr << " ";
+    }
+
+    sort(itr1, itr2);
+    
+    for (vector<int>::iterator itr = itr1; itr != itr2; ++itr)
+    {
+        cout << *itr << " ";
+    }
+    return 0;
+}
