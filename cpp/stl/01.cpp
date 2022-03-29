3890a29f7b9905f75ecbe5ee5e09df318a406145
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-03-29 17:03:53 +0800

    update

--- cpp/stl/01.cpp
+++ cpp/stl/01.cpp
@@ -1,4 +1,14 @@
 # include <vector>
 using namespace std;
 
-vector
+vector<int> vec;
+vec.push_back(4);
+vec.push_back(1);
+vec.push_back(8);
+
+vector<int>::iterator itr1 = vec.begin();
+vector<int>::iterator itr2 = vec.end();
+
+for(vector<int>::iterator itr = itr1;itr!=itr2;++itr)
+    cout<<*itr<<" ";
+sort(itr1,itr2)
