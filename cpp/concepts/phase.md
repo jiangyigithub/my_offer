a15d46cf7a0f392dd31c848b5c88a5dd505d9543
jiangyi <15216787356@163.com>
2022-04-05 19:53:57 +0800

    ++

--- cpp/concepts/phase.md
+++ cpp/concepts/phase.md
@@ -3,7 +3,7 @@ Hello World 是如何运行的
 1. 程序的编译和运行过程
 2. `compile time` and `run time`
 `编译器` `汇编器` `链接器`  
-   ![alt text](materials\compiler.png "Output from ros2")
+   ![alt text](resources\compiler.png "Output from ros2")
 
 - #include 属于编译前的Preprocessing阶段
 - 静态链接和动态链接：程序编译成功，但运行时提示无法找到xxx.dll文件，动态链接库是在run time 时才被调用 
--- cpp/stl/src/algorithm.cpp
+++ cpp/stl/src/algorithm.cpp
@@ -1,27 +1,34 @@
 #include <vector>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <numeric> // some numeric algorithm
+#include <functional>
 using namespace std;
 
-vector<int> vec = { 4, 2, 5, 1, 3, 9};   
-vector<int>::iterator itr = min_element(vec.begin(), vec.end()); // itr -> 1
+int main()
+{
 
-// Note 1: Algorithm always process ranges in a half-open way: [begin, end)
-sort(vec.begin(), itr);  // vec: { 2, 4, 5, 1, 3, 9}
+    vector<int> vec = {4, 2, 5, 1, 3, 9};
+    vector<int>::iterator itr = min_element(vec.begin(), vec.end()); // itr -> 1
 
-reverse(itr, vec.end());  // vec: { 2, 4, 5, 9, 3, 1}   itr => 9
+    // Note 1: Algorithm always process ranges in a half-open way: [begin, end)
+    sort(vec.begin(), itr); // vec: { 2, 4, 5, 1, 3, 9}
 
+    reverse(itr, vec.end()); // vec: { 2, 4, 5, 9, 3, 1}   itr => 9
 
-// Note 2: more than one range loop case
-vector<int> vec2(3);
-copy(itr, vec.end(),  // Source
-     vec2.begin());   // Destination
-     //vec2 needs to have at least space for 3 elements.
+    // Note 2: more than one range loop case
+    vector<int> vec2(3);
+    copy(itr, vec.end(), // Source
+         vec2.begin());  // Destination
+                         // vec2 needs to have at least space for 3 elements.
 
+    // Note 3:
+    vector<int> vec3;
+    copy(itr, vec.end(), back_inserter(vec3)); // Inserting instead of overwriting
+                                               // back_insert_iterator      Not efficient
 
+    vec3.insert(vec3.end(), itr, vec.end()); // Efficient and safe
 
-// Note 3:
-vector<int> vec3;
-copy(itr, vec.end(), back_inserter(vec3));  // Inserting instead of overwriting 
-                  // back_insert_iterator      Not efficient
-
-vec3.insert(vec3.end(), itr, vec.end());  // Efficient and safe
+    return 0;
+}
