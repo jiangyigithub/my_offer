348d5c10aaa6f924437f3b66e7c32bc41cdf984f
JIANG Yi (XC/PJ-ICV-CN) <Yi.JIANG3@cn.bosch.com>
2022-04-11 11:26:37 +0800

    ++

--- cpp/modern/shared_pointer.cpp
+++ cpp/modern/shared_pointer.cpp
@@ -3,47 +3,61 @@
 #include <string>
 using namespace std;
 
-class Dog {
+class Dog
+{
     string m_name;
-  public:
-      void bark() { cout << "Dog " << m_name << " rules!" << endl; }
-      Dog(string name) { cout << "Dog is created: " << name << endl; m_name = name; }
-      Dog() { cout << "Nameless dog created." << endl; m_name = "nameless"; }
-     ~Dog() { cout << "dog is destroyed: " << m_name << endl; }
-	  //void enter(DogHouse* h) { h->setDog(shared_from_this()); }  // Dont's call shared_from_this() in constructor
+
+public:
+    void bark() { cout << "Dog " << m_name << " rules!" << endl; }
+    Dog(string name)
+    {
+        cout << "Dog is created: " << name << endl;
+        m_name = name;
+    }
+    Dog()
+    {
+        cout << "Nameless dog created." << endl;
+        m_name = "nameless";
+    }
+    ~Dog() { cout << "dog is destroyed: " << m_name << endl; }
+    // void enter(DogHouse* h) { h->setDog(shared_from_this()); }  // Dont's call shared_from_this() in constructor
 };
 
 // dangling pointer
-void foo1(){
-    Dog* p=new Dog("Gunner");
+void foo1()
+{
+    Dog *p = new Dog("Gunner");
     //...
     delete p;
     //...
-    p->bark();//p is a dangling pointer - undefined behavior
+    p->bark(); // p is a dangling pointer - undefined behavior
 }
 
 // memory leak
-void foo2(){
-    Dog* p=new Dog("Gunner");
+void foo2()
+{
+    Dog *p = new Dog("Gunner");
     //...
     //...
     p->bark();
-}// Memory leak
+} // Memory leak
 
-void foo3(){
-    shared_ptr<Dog> p(new Dog("Gunner"));
+void foo3()
+{
+    shared_ptr<Dog> p(new Dog("Gunner")); // count = 1
     {
-        shared_ptr<Dog> p2=p;//count = 2
+        shared_ptr<Dog> p2 = p; // count = 2
         p2->bark();
         // stack object
         Dog p3("Stack Gunner");
         // temp object
         Dog("Temp Gunner");
-    }
+    } // count = 1 , out of p2 range
     p->bark();
-}
+} // count = 0, out of p1 range
 
-int main(){
+int main()
+{
     foo3();
     return 0;
 }
